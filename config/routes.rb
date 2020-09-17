637dc26a7ebcf1e2bb57cc44277dd8f499d9198b
Antonio Caeiro <a.caeiro@runtime-revolution.com>
2020-09-17 17:59:02 +0100

    not under vc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :users do
+  	resources :orders
+  end
+  resources :products
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200917162335_create_product_orders.rb
@@ -0,0 +1,9 @@
+class CreateProductOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_join_table :products, :orders do |t|
+    	t.index: product_id
+    	t.index: order_id
+      t.timestamps
+    end
+  end
+end
